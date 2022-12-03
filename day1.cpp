195d916d21fc4dd75e40fa7d18445787a089ef2a
Andrew Morris <andrewzmorris@gmail.com>
2022-12-03 13:57:06 +1100

    Fix build error

--- day1.cpp
+++ day1.cpp
@@ -20,7 +20,7 @@ R"(1000
 auto run_a(std::string_view s) {
     using namespace std::string_view_literals;
     auto elf_total = [](auto elf) { return ranges::accumulate(elf | rv::split("\n") | rv::transform(to_int), 0); };
-    return ranges::max(rv::transform(rv::split(s, rv::split("\n\n"), rv::transform(elf_total))));
+    return ranges::max(rv::transform(rv::split(s, "\n\n")), rv::transform(elf_total));
 }
 
 auto run_b(std::string_view s) {
