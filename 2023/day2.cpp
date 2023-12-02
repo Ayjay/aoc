d0baf1c5fc692ebfa2c00f1aea740999443a5c0f
Andrew Morris <andrewzmorris@gmail.com>
2023-12-02 16:43:09 +1100

    2023 2b

--- 2023/day2.cpp
+++ 2023/day2.cpp
@@ -8,11 +8,11 @@
 #include <boost/fusion/include/std_tuple.hpp>
 
 struct move_t {
-    int red = 0;
-    int green = 0;
-    int blue = 0;
+    long long red = 0;
+    long long green = 0;
+    long long blue = 0;
 };
-using game_t = std::tuple<int, std::vector<move_t>>;
+using game_t = std::tuple<long long, std::vector<move_t>>;
 std::ostream& operator<<(std::ostream& os, move_t m) {
     os << fmt::format("red:{} green:{} blue:{}", m.red, m.green, m.blue);
     return os;
@@ -23,7 +23,7 @@ const auto test_data = std::vector{ std::tuple
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green)", 8, -2}
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green)", 8ll, 2286ll}
 };
 
 const x3::rule<class move_, move_t> move_ = "move";
@@ -31,16 +31,16 @@ const auto set_red = [](auto& ctx) { _val(ctx).red = _attr(ctx); };
 const auto set_green = [](auto& ctx) { _val(ctx).green = _attr(ctx); };
 const auto set_blue = [](auto& ctx) { _val(ctx).blue = _attr(ctx); };
 const auto move__def = 
-    ((int_ >> "red")[set_red]
-  | (int_ >> "blue")[set_blue]
-  | (int_ >> "green")[set_green]) % ',';
+    ((long_long >> "red")[set_red]
+  | (long_long >> "blue")[set_blue]
+  | (long_long >> "green")[set_green]) % ',';
 BOOST_SPIRIT_DEFINE(move_)
 
 auto parse(std::string_view s) {
     auto games = std::vector<game_t>{};
     phrase_parse(
         s.begin(), s.end(),
-        *(lit("Game") >> int_ >> ":" >> (move_ % ';')),
+        *(lit("Game") >> long_long >> ":" >> (move_ % ';')),
         space, games);
     return games;
 }
@@ -57,7 +57,20 @@ auto run_a(std::string_view s) {
 }
 
 auto run_b(std::string_view s) {
-    return -1;
+    const auto games = parse(s);
+    const auto get_minimum_cubes = [](const game_t& game) {
+        return reduce(std::get<1>(game), [](auto a, auto b) {
+            return move_t{ std::max(a.red,b.red),std::max(a.green,b.green),std::max(a.blue,b.blue) };
+        });
+    };
+    const auto get_power = [](move_t move) {
+        return move.red * move.green * move.blue;
+    };
+    return reduce(
+        games
+        | rv::transform(get_minimum_cubes)
+        | rv::transform(get_power)
+    );
 }
 
 int main() {
