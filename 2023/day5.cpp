aa72f1319a1e1c75568ad6229db8bd67afc26850
Andrew Morris <andrewzmorris@gmail.com>
2023-12-07 02:00:03 +0000

    2023 day5a

--- 2023/day5.cpp
+++ 2023/day5.cpp
@@ -1,4 +1,4 @@
-#define BOOST_SPIRIT_X3_DEBUG
+//#define BOOST_SPIRIT_X3_DEBUG
 #include "aoc.h"
 
 #include <vector>
@@ -10,6 +10,7 @@
 
 #include <boost/unordered_map.hpp>
 #include <boost/icl/interval_map.hpp>
+#include <fmt/ostream.h>
 
 const auto test_data = std::vector{ std::tuple
 {R"(seeds: 79 14 55 13
@@ -48,7 +49,9 @@ humidity-to-location map:
 };
 
 using namespace boost::icl;
-using mapping = interval_map<int, int>;
+using mapping = interval_map<long long, long long>;
+template <> struct fmt::formatter<mapping> : ostream_formatter {};
+
 using ascii::alpha;
 
 const x3::rule<class mapping_, mapping> mapping_ = "mapping";
@@ -56,38 +59,59 @@ const auto add_mapping = [](auto& ctx) {
     auto dest = at_c<0>(_attr(ctx));
     auto source = at_c<1>(_attr(ctx));
     auto length = at_c<2>(_attr(ctx));
-    _val(ctx).add(std::make_pair(interval<int>::right_open(source, source + length), source - dest));
+    _val(ctx).add(std::make_pair(interval<long long>::right_open(source, source + length), dest - source));
 };
-const auto mapping__def = *((int_ >> int_ >> int_)[add_mapping]);
+const auto mapping__def = *(long_long >> long_long >> long_long)[add_mapping];
 BOOST_SPIRIT_DEFINE(mapping_);
+
+auto get_mapping(const mapping& map, long long index) {
+    if (auto it = map.find(index); it != map.end())
+        return index + it->second;
+    return index;
+}
 
 auto parse(std::string_view s) {
     auto it = s.begin();
-    auto seeds = std::vector<int>{};
+    auto seeds = std::vector<long long>{};
     phrase_parse(it, s.end(),
-        "seeds: " >> *int_,
+        "seeds: " >> *long_long,
         space, seeds);
 
-    auto raw_maps = std::tuple<std::string, std::string>{};
+    auto raw_maps = std::vector<std::tuple<std::string, std::string, mapping>>{};
     phrase_parse(it, s.end(),
-        *alpha >> "-to-" >> *alpha >> "map:",
+        *(*alpha >> "-to-" >> x3::lexeme[*alpha] >> "map:" >> mapping_),
         space, raw_maps);
 
-    //auto maps = raw_maps 
-    //    | rv::transform([](auto ms) {
-    //        auto [from, to, m] = ms;
-    //        return std::tuple{ from, std::tuple{to,m} };
-    //    })
-    //    | ranges::to<std::unordered_map<std::string, std::tuple<std::string, mapping>>>;
-    //return std::tuple{ seeds, maps };
-    return seeds;
+    auto maps = raw_maps 
+        | rv::transform([](auto ms) {
+            auto [from, to, m] = ms;
+            auto get_mapping = [map=std::move(m)](long long index) {
+                if (auto it = map.find(index); it != map.end())
+                    return index + it->second;
+                return index;
+            };
+            return std::pair{ from, std::tuple{to,std::move(get_mapping)} };
+        })
+        | ranges::to<std::unordered_map>;
+    return std::tuple{ seeds, maps };
 }
 
-auto run_a(std::string_view s) {
-    //const auto [seeds, maps] = parse(s);
-    parse(s);
+auto get_location(const auto& maps, long long seed, const auto& type) {
+    const auto& [to, map] = maps.at(type);
+    if (to == "location")
+        return map(seed);
+    else
+        return get_location(maps, map(seed), to);
+};
 
-    return -1;
+auto run_a(std::string_view s) {
+    const auto [seeds, maps] = parse(s);
+
+    const auto locations = seeds
+       | rv::transform([&](auto seed) { return get_location(maps, seed, "seed"); })
+       | ranges::to<std::vector>;
+
+    return *ranges::min_element(locations);
 }
 
 auto run_b(std::string_view s) {
