cbad42f610149122a6f756e39c89fb2bb9593936
Andrew Morris <andrewzmorris@gmail.com>
2023-12-20 05:49:40 +0000

    Work?

--- 2023/day17.cpp
+++ 2023/day17.cpp
@@ -59,44 +59,48 @@ std::size_t hash_value(const state_t& state) {
 auto get_min_loss(const auto& grid) {
     const auto [max_rows,max_cols] = get_bounds(grid);
     const auto in_bounds = make_in_bounds(grid);
-
-    auto s = std::stack<state_t>{};
-    s.push(state_t{point_t{1,0}, south, 0, 0});
-    s.push(state_t{point_t{0,1}, east, 0, 0});
     const auto max_straight_line = 3;
-    auto cache = boost::multi_array<long long, 4>{boost::extents[max_rows][max_cols][directions.size()][max_straight_line]};
+
+    using move_t = std::tuple<state_t,state_t>;
+    auto s = std::stack<move_t>{};
+    s.push({state_t{point_t{0,0}, south, 0, 0}, state_t{point_t{1,0}, south, 1, 0}});
+    s.push({state_t{point_t{0,0}, east, 0, 0}, state_t{point_t{0,1}, east, 1, 0}});
+
+    auto cache = boost::multi_array<std::optional<move_t>{boost::extents[max_rows][max_cols][directions.size()][max_straight_line]};
+    const auto get_cached_value = [&](state_t state) -> decltype(auto) {
+        const auto [row,col] = state.point;
+        return cache[row][col][direction_to_index(state.facing)][state.straight_line_distance];
+    };
 
     while (!s.empty()) {
-        const auto state = s.top();
+        const auto [from,to] = s.top();
         s.pop();
 
-        const auto [row,col] = state.point;
+        const auto [to_row,to_col] = to.point;
 
-        const auto weight = state.accumulated_weight + (grid[row][col] - '0');
-        const auto get_cached_value = [&](state_t state) -> decltype(auto) {
-            return cache[row][col][direction_to_index(state.facing)][state.straight_line_distance];
-        };
+        const auto weight = to.accumulated_weight + (grid[to_row][to_col] - '0');
         auto& cached = get_cached_value(state);
-        if (cached != 0 && cached < weight)
-            continue;
+        if (cached) {
+            const auto& [cached_from, cached_to] = *cached;
+            if (cached_to.accumulated_weight < weight)
+                continue;
+        }
 
-        cached = weight;
+        cached = move_t{from,to};
         if (row == max_rows - 1 && col == max_cols - 1) {
             continue;
         }
 
         for (const auto direction : directions) {
-            if (direction == opposite(state.facing))
+            if (direction == opposite(to.facing))
                 continue;
 
-            const auto new_p = add(state.point, direction);
+            const auto new_p = add(to.point, direction);
             if (!in_bounds(new_p))
                 continue;
 
             const auto add_state = [&](state_t state) {
-                const auto& cached = get_cached_value(state);
-                if (cached == 0 || cached > state.accumulated_weight)
-                    s.push(state);
+                s.push({to,state});
             };
 
             if (direction == state.facing) {
@@ -108,18 +112,21 @@ auto get_min_loss(const auto& grid) {
         }
     }
 
-    for (const auto row : rv::iota(0, max_rows)) {
-        auto mins = std::vector<long long>{};
-        for (const auto col : rv::iota(0, max_cols)) {
-            mins.push_back(9999999);
-            for (const auto direction : rv::iota(0u, directions.size())) {
-                for (auto i : cache[row][col][direction])
-                    if (i != 0)
-                        mins.back() = std::min(i, mins.back());
-            }
-        }
-        fmt::println("{}", mins);
-    }
+    // for (const auto row : rv::iota(0, max_rows)) {
+    //     auto mins = std::vector<long long>{};
+    //     for (const auto col : rv::iota(0, max_cols)) {
+    //         mins.push_back(9999999);
+    //         for (const auto direction : rv::iota(0u, directions.size())) {
+    //             for (auto cached : cache[row][col][direction]) {
+    //                 if (cached) {
+    //                     const auto [from,to] = *cached;
+    //                     mins.back() = std::min(i, mins.back());
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     fmt::println("{}", mins);
+    // }
 
     // auto first_cells = cache | rv::filter([&](const auto& kv) { 
     //     const auto& [state,weight] = kv;
@@ -128,9 +135,12 @@ auto get_min_loss(const auto& grid) {
     // });
     auto min = std::optional<long long>{};
     for (const auto direction : rv::iota(0u, directions.size())) {
-        for (auto n : cache[max_rows-1][max_cols-1][direction]) {
-            if (n != 0 && (!min || n < *min))
-                min = n;
+        for (auto cached : cache[max_rows-1][max_cols-1][direction]) {
+            if (cached) {
+                const auto [from,to] = *cached;
+                if (!min || to.accumulated_weight < *min)
+                    min = to.accumulated_weight;
+            }
         }
     }
     return *min;
