9d6dc46220bf24b7dcbb2f09159baa45d72ee167
Andrew Morris <andrewzmorris@gmail.com>
2024-12-12 20:12:35 +0000

    day8b

--- 2024/grid.hpp
+++ 2024/grid.hpp
@@ -25,6 +25,10 @@ inline vector2 operator-(vector2 a, vector2 b) {
     const auto [b_r,b_c] = b;
     return {a_r-b_r,a_c-b_c};
 }
+inline vector2 operator*(vector2 a, auto constant) {
+    const auto [a_r,a_c] = a;
+    return {a_r*constant,a_c*constant};
+}
 
 inline auto indexer(const auto& map) {
     return [&](const vector2 p) {
